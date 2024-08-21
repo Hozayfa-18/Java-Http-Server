2147a3ca26f36d9dcb5790bfcf975db41bd8b5cd
Hozayfa-18 <hozayfa2020h@gmail.com>
2024-08-21 12:56:17 +0200

    updated

--- src/main/java/com/hozayfa/httpserver/HttpServer.java
+++ src/main/java/com/hozayfa/httpserver/HttpServer.java
@@ -8,6 +8,12 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HttpServer {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
@@ -28,5 +34,28 @@ public class HttpServer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try {
+            JasperReport jasperReport = JasperCompileManager.compileReport("C:/Users/hozay/JaspersoftWorkspace/MyReports/einkaufsliste_report.jrxml");
+            List<Item> employees = getItems();
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employees);
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("ReportTitle", "Employee Report");
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, "EinkaufslisteReport.pdf");
+
+            System.out.println("Report generated!");
+
+        } catch (JRException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Item> getItems() {
+        return List.of(
+                new Item("Bannana", 2, 6),
+                new Item("Orange", 1, 5),
+                new Item("Chips", 2, 2)
+        );
     }
 }
---
+++ src/main/java/com/hozayfa/httpserver/Item.java
@@ -0,0 +1,13 @@
+package com.hozayfa.httpserver;
+
+public class Item {
+    String name = null;
+    int price = 0;
+    int amount =0;
+
+    public Item(String name, int price, int amount){
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+}
